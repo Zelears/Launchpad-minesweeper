97ed81312d5c57efa3f521edfde6a919bfeb5579
Piotr Skłodowski <Zelearsskl@gmail.com>
2020-01-23 12:43:20 +0100

    Upload

---
+++ lib/minesweeper.rb
@@ -0,0 +1,29 @@
+$:.unshift(Dir.pwd)
+require 'ruby2d'
+require 'pry'
+require 'unimidi'
+require_relative 'board'
+
+board = Board.new
+set title: "Minesweeper", background: "random"
+
+on :key do |event|
+  if event.type == :down
+    case event.key
+    when "w"
+      board.clear_board
+    when "q"
+      board.regenerate_board
+    when "escape"
+      board.clear_board
+      board.clear_top_panel
+      close
+    end
+  end
+end
+
+update do
+  board.input_check?
+end
+
+show
